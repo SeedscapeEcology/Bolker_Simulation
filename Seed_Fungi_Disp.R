78ba0e2d122e6430fe6444494dc1c34296c00b77
Unknown <e.sodja816@gmail.com>
2018-04-08 19:30:03 -0600

    Updates for presentation creation

--- Seed_Fungi_Disp.R
+++ Seed_Fungi_Disp.R
@@ -1,7 +1,7 @@
-set.seed(1001) # setting the random number generator
+## set.seed(1001) # setting the random number generator
 L <- 30 # maximum range
-nparents <- 50 # number of starting parents
-offspr_per_parent <- 10 # yup
+nparents <- 10 # number of starting parents
+offspr_per_parent <- 100 # yup
 noffspr <- nparents * offspr_per_parent # total offspring
 dispdist <- 2 # dispersal distance
 
@@ -20,21 +20,21 @@ offspr_y <- rep(parent_y, each = offspr_per_parent) + sin(angle) * dist # same a
 offspr_pos <- data.frame(offspr_x=offspr_x, offspr_y=offspr_y) # binding the x and y values together
 
 ### Fungal Dispersal ###
-f_colonies <- 10
-f_spores <- 100 # it's probably a lot more(?)
-f_dispdist <- 2
+f_colonies <- 5
+f_spores <- 200 # it's probably a lot more(?)
+f_dispdist <- .5
 p_infection <- 0.25
 f_movedist <- 5
 
 # Assuming the fungal pathogens depend on parent trees to survive?
 fparent_xy <- parent_xy[sample(nrow(parent_xy), f_colonies), ] # starting location of fungi is on existing parent trees
-names(fparent_xy)[names(fparent_xy)=="parent_x"] <- "fparent_x"
-names(fparent_xy)[names(fparent_xy)=="parent_y"] <- "fparent_y"  # renaming the columns for consistency
+#names(fparent_xy)[names(fparent_xy)=="x"] <- "fparent_x"
+#names(fparent_xy)[names(fparent_xy)=="y"] <- "fparent_y"  # renaming the columns for consistency
 
 fangle <- runif(f_spores, min = 0, max = 2*pi) # list of angles to shoot off the spores
 fdist <- rexp(f_spores, 1/f_dispdist) # distances to shoot off spores
-foffspr_x <- rep(fparent_xy$fparent_x, each = f_spores) + cos(fangle) * fdist
-foffspr_y <- rep(fparent_xy$fparent_y, each = f_spores) + sin(fangle) * fdist
+foffspr_x <- rep(fparent_xy$x, each = f_spores) + cos(fangle) * fdist
+foffspr_y <- rep(fparent_xy$y, each = f_spores) + sin(fangle) * fdist
 
 foffspr_xy <- data.frame(foffspr_x=foffspr_x, foffspr_y=foffspr_y) # landing locations of spores
 
@@ -48,22 +48,28 @@ names(fparent_xy) <- c("x", "y")
 names(foffspr_xy) <- c("x", "y")
 fungiloc <- rbind(fparent_xy, foffspr_xy) # all fungi locations
 
+plot(parent_xy, col="black")
+points(offspr_pos, col="green")
+points(fparent_xy, col="red")
+points(foffspr_xy, col="orange")
+
+
 pythagdist <- function(x1, x2, y1, y2){
-  xdist2 = (x1-x2)^2
-  ydist2 = (y1-y2)^2
-  zdist = sqrt(xdist2+ydist2)
-  return(zdist)
+    xdist2 = (x1-x2)^2
+    ydist2 = (y1-y2)^2
+    zdist = sqrt(xdist2+ydist2)
+    return(zdist)
 }
 
 #testlist <- pythagdist(treeloc$x, fungiloc$x, treeloc$y, fungiloc$y)
 
 for (i in treeloc$x){
-  for (j in fungiloc$x){
-    if (pythagdist(treeloc$x[i], fungiloc$x[j], treeloc$y[i], fungiloc$y[j]) < f_movedist){
-      return(treeloc$x[i])
-      return(treeloc$y[i])
+    for (j in fungiloc$x){
+        if (pythagdist(treeloc$x[i], fungiloc$x[j], treeloc$y[i], fungiloc$y[j]) < f_movedist){
+            return(treeloc$x[i])
+            return(treeloc$y[i])
+        }
     }
-  }
 }
 # reset fungus to infected trees
 
